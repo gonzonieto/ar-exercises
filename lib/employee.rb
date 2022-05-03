87c4ed06c31c5b042ae961e0ddae911449e95601
‘Gonzo <gonzonieto@gmail.com>
2022-05-03 20:29:52 +0000

    update Employee and Store and add four Employees

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,8 @@
 class Employee < ActiveRecord::Base
+  belongs_to :store
 
+  @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+  @store1.employees.create(first_name: "Gonzo", last_name: "Nieto", hourly_rate: 150)
+  @store1.employees.create(first_name: "Jeff", last_name: "Bezos", hourly_rate: 13)
+  @store1.employees.create(first_name: "Archie", last_name: "Fuller", hourly_rate: 14)
 end
