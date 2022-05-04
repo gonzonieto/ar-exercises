8f04fee65949104d1fe7e0c16298a79e00ee65bd
‘Gonzo <gonzonieto@gmail.com>
2022-05-04 02:11:03 +0000

    update validations

--- lib/employee.rb
+++ lib/employee.rb
@@ -2,5 +2,10 @@ class Employee < ActiveRecord::Base
   belongs_to :store
   validates_associated :store
   validates :first_name, :last_name, presence: true
-  validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+  validates :hourly_rate, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
 end
+
+# - Employees must always have a first name present
+# - Employees must always have a last name present
+# - Employees have a hourly_rate that is a number (integer) between 40 and 200
+# - Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, presence: true, length: { minimum: 3, message: 'Store must have a name with a minimum of three characters.'}
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0, message: 'Store must have a revenue that is an integer greater than 0' }
+  validate :must_carry_at_least_mens_or_womens_apparel
+
+  def must_carry_at_least_mens_or_womens_apparel
+    unless [mens_apparel, womens_apparel].any?
+      errors.add(:womens_apparel, "Does not sell women's apparel; must sell at least one of men's or women's apparel")
+      errors.add(:mens_apparel, "Does not sell men's apparel; must sell at least one of men's or women's apparel")
+    end
+  end
 end
